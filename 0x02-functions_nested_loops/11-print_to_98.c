085802f600135986f45b732aed279dca9b69a3da
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-02-22 17:08:30 -0800

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,14 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			_putchar( n + '0' );
+			_putchar( n + 0 );
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
-			_putchar( n + '0' );
+			_putchar( n + 0 );
 		}
 	}
 	_putchar('\n');
