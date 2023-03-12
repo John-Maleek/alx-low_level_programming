2c5e882dbb1b4cfdd70977c6750914a728bf8999
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-03-12 14:23:59 -0700

    tasks 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - prints the number of arguments passed into it.
+ * main - print the result of the multiplication, followed by a new line.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int product;
 	if (argc == 3)
