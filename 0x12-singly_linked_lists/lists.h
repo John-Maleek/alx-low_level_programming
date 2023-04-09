069d8667e0ebc35c079f93ce8f82f21849ae4137
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-04-09 11:59:03 -0700

    tasks on linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,18 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
     char *str;
