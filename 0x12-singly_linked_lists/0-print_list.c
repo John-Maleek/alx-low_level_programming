3f9b3ecda6561c98b4ae839394b824ef1618af11
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-04-09 12:12:54 -0700

    tasks on linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,24 +6,20 @@
 /**
  *print_list: prints all elements of a list
  *
- * Return: number of nodes
+ * Return: number of items in the linked list
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t list_len;
-	list_len = 0;
 	list_t *curr = h;
+	size_t list_len;
+	
+	list_len = 0;
    	while (curr != NULL)
 	{
-		if(curr->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else
-		{
-			printf("[%d] %s\n", curr->len, curr->str);
-		}
+		if (curr->str == NULL)
+			printf("[%d] %s\n", 0, "(nill)");
+		printf("[%d] %s\n", curr->len, curr->str);
 		curr = curr->next;
 		list_len++;
     	}
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  *list_len: returns the number of elements in a list
