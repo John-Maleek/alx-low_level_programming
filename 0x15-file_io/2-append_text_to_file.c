b73a83da450cc15a776b21469aa0811115ae4fc7
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-05-09 21:46:26 -0700

    my first commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 
-		for (text_len = 0; text_content[text_len}; text_len++)
+		for (text_len = 0; text_content[text_len]; text_len++)
 			;
 
 		write_res = write(fd, text_content, text_len);
