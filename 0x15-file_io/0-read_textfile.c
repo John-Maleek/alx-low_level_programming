13ebe3d4e954da7e4c674ad7538251b80e56c093
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-07-14 16:32:03 -0700

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,8 +3,10 @@
 /**
  *read_textfile - a function that reads a text file and
  *prints it to the POSIX standard output
- *filename: name of fiile to read from
- *letters: number of letters to be read
+ *
+ *@filename: name of fiile to read from
+ *@letters: number of letters to be read
+ *
  *Return: the actual number of letters it could read and print
  */
 
@@ -17,19 +19,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 	{
 		return (0);
 	}
-
 	read_res = read(fd, buff, letters);
 	if (read_res == -1)
 	{
@@ -37,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	write_res = write(STDOUT_FILENO, buff, read_res);
 	if (write_res == -1)
 	{
