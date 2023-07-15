dd8ec02bdf01a6d092a75cb396534cc1ae7a8cc9
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-07-14 17:51:01 -0700

    third task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,12 +29,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	int i = 0;
+	if (text_content)
+	{
+		int i = 0;
 
-	while (text_content[i])
-		i++;
+		while (text_content[i])
+			i++;
+		write_res = write(fd, text_content, i);
+	}
 
-	write_res = write(fd, text_content, i);
 	if (write_res == -1)
 	{
 		close(fd);
